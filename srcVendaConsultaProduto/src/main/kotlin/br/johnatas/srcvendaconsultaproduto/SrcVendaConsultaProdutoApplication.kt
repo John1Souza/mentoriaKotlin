474a000f760b2e389327d637c5e398618e1f19fd
John1Souza <johnatas2014@hotmail.com>
2023-12-18 14:41:58 -0300

    [NEW] First commit

---
+++ srcVendaConsultaProduto/src/main/kotlin/br/johnatas/srcvendaconsultaproduto/SrcVendaConsultaProdutoApplication.kt
@@ -0,0 +1,11 @@
+package br.johnatas.srcvendaconsultaproduto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SrcVendaConsultaProdutoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SrcVendaConsultaProdutoApplication>(*args)
+}
